3bced1e960e156c6ed24c31b1fb306af9e73f051
Nick Newberg <nick@spaces.com>
2019-01-18 09:51:24 -0800

    initial unity proj commit

---
+++ chronicle-unity/Scrap/Assets/InputController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.MagicLeap;
+
+public class InputController : MonoBehaviour {
+    
+    private MLInputController controller;
+    private Spawner spawner;
+
+    void Awake()
+    {
+        MLInput.Start();
+        controller = MLInput.GetController(MLInput.Hand.Left);
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
