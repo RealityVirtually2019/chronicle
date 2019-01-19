af1aa95d7193f6de64da372c2b4d243768bed4a2
Gaston Montemayor <gastonmo@spotify.com>
2019-01-18 19:08:56 -0500

    Add simple server

--- chronicle-ios/ChronicleApp/app/screens/Text.js
+++ chronicle-ios/ChronicleApp/app/screens/Text.js
@@ -1,12 +1,36 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Button, TextInput, View } from 'react-native';
 
 export default class TextScreen extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { text: '' };
+  }
+
   render() {
+    
+    const onPressSubmit = () => {
+      makeRequest(this.state.text);
+      this.setState({ text: '' });
+    }
+
     return (
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>This is some text</Text>
+        <TextInput
+          style={{height: 40, width: '80%', borderColor: 'gray', borderWidth: 1, padding: '2.5%'}}
+          placeholder='Write anything...'
+          onChangeText={(text) => this.setState({text})}
+          value={this.state.text}
+        />
+        <Button
+          onPress={onPressSubmit}
+          title="Submit"
+        />
       </View>
     );
   }
+}
+
+const makeRequest = (textString) => {
+  
 }
