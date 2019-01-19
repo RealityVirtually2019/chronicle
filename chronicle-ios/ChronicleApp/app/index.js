1fa28cfba0721a3e697f19fa9f5b734e5bd167f1
gpmo <gastonmo@spotify.com>
2019-01-18 23:50:10 -0500

    Add correct username

--- chronicle-ios/ChronicleApp/app/index.js
+++ chronicle-ios/ChronicleApp/app/index.js
@@ -5,7 +5,7 @@ import io from 'socket.io-client';
 class App extends Component {
   constructor(props) {
     super(props);
-    this.socket = io('https://chronicle-client-server.herokuapp.com/');
+    this.socket = io('https://chronicle-client-server.herokuapp.com');
   }
   render() {
     return <AppContainer screenProps={{socket: this.socket}}/>;
