e8d05b123427b6eaff26f8a42200ddd26ddc12f4
gpmo <gastonmontemayor@gmail.com>
2019-01-19 17:49:41 -0500

    Add final endpoints for RESTful polling :)

--- chronicle-server/app.js
+++ chronicle-server/app.js
@@ -4,18 +4,30 @@ const http = require('http').Server(app);
 const storage = require('node-persist');
 
 const port = process.env.PORT || 8080;
+app.use(bodyParser.json());
 
-app.get('/ping', async (req, res) => {
-  //you must first call storage.init
-  await storage.init( /* options ... */ );
-  await storage.setItem('name','yourname')
-  const name = await storage.getItem('name')
-  console.log(await storage.getItem('name')); // yourname
+app.get('/ping', (req, res) => {
   res.send(name);
 });
 
-app.post('/chronicle/text', (req, res) => {
-  console.log(req.body);
+app.post('/chronicle/text', async (req, res) => {
+  await storage.init();
+  const storageItem = req.body.data;
+  console.log('Setting item to latest: ' + storageItem);
+  await storage.setItem('latest', {type: 'text', data: storageItem});
+});
+
+app.get('/latest', async (req, res) => {
+  await storage.init();
+  const storageItem = await storage.getItem('latest');
+  if (storageItem === undefined) {
+    console.log('No new items');
+    res.send('');
+  } else {
+    console.log('Found latest item: '  + storageItem.data + ' with type ' + storageItem.type); 
+    await storage.removeItem('latest');
+    res.send(storageItem);
+  }
 });
 
 http.listen(port, () => {
